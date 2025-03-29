81d394a4fb525d64044095907d1aafe9c37ba859
cosmin <cosmin_popovici2012@yahoo.com>
2025-03-29 14:00:08 +0200

    Added input file and input read code

--- ParallelProgramming/ParallelProgramming/Source.cpp
+++ ParallelProgramming/ParallelProgramming/Source.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <filesystem>
 #include <mpi.h>
+#include <fstream>
+#include <thread>
 
-int main()
+std::vector<int> readFromFile(const std::string& filename) {
+	std::vector<int> numbers;
+	std::ifstream inFile(filename);
+	if (inFile.is_open()) {
+		int num;
+		while (inFile >> num) {
+			numbers.push_back(num);
+		}
+		inFile.close();
+	}
+	else {
+		std::cerr << "Error opening file!" << std::endl;
+	}
+	return numbers;
+}
+
+int main(int argc, char** argv)
 {
-	
+	MPI_Init(&argc, &argv);
+
+	double time = MPI_Wtime();
+
+	std::vector<int> numbers = readFromFile("input.txt");
+
+	time = MPI_Wtime() - time;
+
+	std::cout << "Time to read file: " << time << std::endl;
+
 	return 0;
 }
